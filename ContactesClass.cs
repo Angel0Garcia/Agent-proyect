18382b2e14584ef1ed3249c07d9f975275242f33
Angel <gabrielg080nac@gmail.com>
2024-02-16 08:41:18 -0400

    First Commit

---
+++ ContactesClass.cs
@@ -0,0 +1,99 @@
+﻿
+
+//Console.WriteLine("Mi Agenda Perrón");
+//Console.WriteLine("Bienvenido a tu lista de contactes");
+
+//Dictionary<int, string> names = new Dictionary<int, string>();
+//Dictionary<int, string> phones = new Dictionary<int, string>();
+//Dictionary<int, string> emails = new Dictionary<int, string>();
+//List<string> addresses = new List<string>();
+//List<int> ids = new List<int>();
+
+//bool running = true;
+//while (running)
+//{
+
+//    Console.Write("1. Agregar Contacto      ");
+//    Console.Write("2. Ver Contactos     ");
+//    Console.Write("3. Buscar Contactos      ");
+//    Console.Write("4. Modificar Contacto        ");
+//    Console.Write("5. Eliminar Contacto     ");
+//    Console.WriteLine("6. Salir");
+//    Console.Write("Elige una opción: ");
+
+//    int choice = Convert.ToInt32(Console.ReadLine());
+
+//    switch (choice)
+//    {
+//        case 1:
+//            AddContact(ref names, ref phones, ref emails, ref addresses, ref ids);
+//            break;
+//        case 2:
+//            ViewContacts(ref names, ref phones, ref emails, ref ids);
+//            break;
+//        case 4:
+//            EditContact();
+//            break;
+//        case 5:
+//            DeleteContact();
+//            break;
+//        case 3: //esto es intencional, no importa el orden en que pongan los cases, pero, traten de ser siempre lo mas ordenados posible
+//            SearchContact();
+//            break;
+//        case 6:
+//            running = false;
+//            break;
+//        default:
+//            Console.WriteLine("Opción no válida");
+//            break;
+//    }
+//}
+
+//static void AddContact(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<string> addresses, ref List<int> ids)
+//{
+//    Console.WriteLine("Vamos a agregar ese contacte que te trae loco.");
+
+//    int id = ids.Count() + 1;
+//    ids.Add(id);
+//    Console.Write("Digite el Nombre: ");
+//    var name = Console.ReadLine();
+//    names.Add(id, name);
+//    Console.Write("Digite el Teléfono: ");
+//    var telefono = Console.ReadLine();
+//    phones.Add(id, telefono);
+
+//    Console.Write("Digite el Email: ");
+//    var email = Console.ReadLine();
+//    emails.Add(id, email);
+//    Console.Write("Digite la dirección: ");
+//    var address = Console.ReadLine();
+//    addresses.Add(address);
+//    Console.WriteLine();
+
+//}
+
+//static void ViewContacts(ref Dictionary<int, string> names, ref Dictionary<int, string> phones, ref Dictionary<int, string> emails, ref List<int> ids)
+//{
+//    Console.WriteLine("Id           Nombre          Telefono            Email");
+//    Console.WriteLine("___________________________________________");
+
+//    foreach (var id in ids)
+//    {
+//        Console.WriteLine($"{id}        {names[id]}      {phones[id]}      {emails[id]}");
+//    }
+//}
+
+//static void EditContact()
+//{
+//    // Implementar lógica para editar un contacto.
+//}
+
+//static void DeleteContact()
+//{
+//    // Implementar lógica para eliminar un contacto.
+//}
+
+//static void SearchContact()
+//{
+//    // Implementar lógica para eliminar un contacto.
+//}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿//Diccionarios para almacenar los datos de los contactos 
+Dictionary<int, string> nombres = new Dictionary<int, string>();
+Dictionary<int, string> correos = new Dictionary<int, string>();
+Dictionary<int, string> numeros = new Dictionary<int, string>();
+Dictionary<int, string> direcciones = new Dictionary<int, string>();
+List<int> ids = new List<int>();
+
+//Menu del usuario
+Console.WriteLine("Mi agenda perróna.");
+bool activo = true;
+
+while (activo)
+{
+    Console.WriteLine("Bienvenido a la agenda, que desa hacer?");
+    Console.WriteLine("1.Agregar Contacto 2.Mostrar contactos 3.Editar Contacto 4.Eliminar Contacto 5.Salir ");
+    Console.Write("Digite la opcion elegida: > ");
+
+    int select = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" ");
+
+
+
+    switch (select)
+    {
+        case 1:
+            addContacs(ref nombres, ref numeros, ref correos, ref direcciones, ref ids);
+            break;
+        case 2:
+            viewContacs(ref nombres, ref numeros, ref correos, ref ids);
+            break;
+        case 3:
+            editContacs(ref nombres, ref numeros, ref correos, ref direcciones, ref ids);
+            break;
+        case 4:
+            deleteContacs(ref nombres, ref numeros, ref correos, ref direcciones, ref ids);
+            break;
+        case 5:
+            activo = false;
+            break;
+        default:
+            Console.WriteLine("No es una opcion");
+            break;
+    }
+}
+static void addContacs(ref Dictionary<int, string> nombres, ref Dictionary<int, string> numeros, ref Dictionary<int, string> correos, ref Dictionary<int, string> direcciones, ref List<int> ids)
+{
+    int id = ids.Count() + 1;
+    ids.Add(id);
+    Console.WriteLine("Agregar contacto");
+    Console.Write("Nombre: ");
+    var nombre = Console.ReadLine();
+    nombres.Add(id, nombre);
+    Console.Write("Numero de telefono: ");
+    var numero = Console.ReadLine();
+    numeros.Add(id, numero);
+    Console.Write("Direccion de Correo Electronico: ");
+    var correo = Console.ReadLine();
+    correos.Add(id, correo);
+    Console.Write("Direccion de residencia: ");
+    var direccion = Console.ReadLine();
+    direcciones.Add(id, direccion);
+    Console.WriteLine(" ");
+}
+
+static void viewContacs(ref Dictionary<int, string> nombres, ref Dictionary<int, string> numeros, ref Dictionary<int, string> correos, ref List<int> ids)
+{
+    Console.WriteLine("Estos son los contactos agregados");
+    Console.WriteLine("---------------------------------");
+
+    foreach (var id in ids)
+    {
+        Console.WriteLine("  Id:             Nombre:            Numero:            Correo:     ");
+        Console.WriteLine($"{ids}       {nombres[id]}      {numeros[id]}      {correos[id]}");
+        Console.WriteLine();
+    }
+}
+
+static void editContacs(ref Dictionary<int, string> nombres, ref Dictionary<int, string> numeros, ref Dictionary<int, string> direcciones, ref Dictionary<int, string> correos, ref List<int> ids)
+{
+    
+}
+
+static void deleteContacs(ref Dictionary<int, string> nombres, ref Dictionary<int, string> numeros, ref Dictionary<int, string> direcciones, ref Dictionary<int, string> correos, ref List<int> ids)
+{
+    
+}
